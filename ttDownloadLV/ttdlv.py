54fb5de132e5eecbde26857f107a73e2d4fb3911
Jose Benhumea <joseramosb@gmail.com>
2023-01-06 23:47:50 -0600

    Version 0.0.1 la cual arroja diferentes errores que no son resueltos

--- ttDownloadLV/ttdlv.py
+++ ttDownloadLV/ttdlv.py
@@ -1,59 +1,56 @@
-# Instala Selenium
-# pip install selenium
+import requests
+import os
 
-# Importa las librerías necesarias
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+# Obtén tu "client key" y "client secret" de la consola de desarrolladores de TikTok
+CLIENT_KEY = "aw6pq0ii3ved3nox"
+CLIENT_SECRET = "59b0d9b483dc41ac515377e3a5bc62f8"
 
-# Crea una función para iniciar sesión en TikTok
-def login_to_tiktok(username, password):
-    # Crea un objeto webdriver y abre TikTok
-    driver = webdriver.Firefox()
-    driver.get("https://www.tiktok.com/")
+# Solicita un "access token" a la API de TikTok
+response = requests.post("https://api.tiktok.com/oauth/token", {
+    "client_key": CLIENT_KEY,
+    "client_secret": CLIENT_SECRET,
+    "grant_type": "client_credentials",
+    "scope": "favorite"
+})
 
-    # Haz clic en el botón "Iniciar sesión"
-    login_button = driver.find_element_by_xpath("//a[@href='/login/email']")
-    login_button.click()
-
-    # Introduce tu nombre de usuario y contraseña
-    username_field = driver.find_element_by_xpath("//input[@name='email']")
-    password_field = driver.find_element_by_xpath("//input[@name='password']")
-    username_field.send_keys(username)
-    password_field.send_keys(password)
-
-    # Haz clic en el botón "Iniciar sesión"
-    login_button = driver.find_element_by_xpath("//button[@type='submit']")
-    login_button.click()
-    
-    # Espera a que la página de inicio de sesión cargue
-    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//a[@href='/@tuusuario']")))
-
-# Inicia sesión en TikTok utilizando tu nombre de usuario y contraseña
-login_to_tiktok("joseramosb@gmail.com", "j0s33l173")
-
-# Accede a la página de videos que le has dado "me gusta"
-driver.get("https://www.tiktok.com/favorite/list")
+# Obtén el "access token" de la respuesta
+access_token = response.json()["access_token"]
 
 # Crea una carpeta para guardar los videos descargados
 if not os.path.exists("videos"):
     os.makedirs("videos")
 
-# Obtén la lista de elementos de video de la página
-video_elements = driver.find_elements_by_xpath("//a[@title='Video']")
+# Obtén la lista de videos que le has dado "me gusta" en tu cuenta de TikTok
+url = "https://api.tiktok.com/favorite/list"
+headers = {
+    "Authorization": f"Bearer {access_token}"
+}
+params = {
+    "count": "100"  # Solicita 100 videos por página
+}
+
+# Realiza la solicitud a la API
+response = requests.get(url, headers=headers, params=params)
+
+# Obtén los datos de los videos de la respuesta
+videos = response.json()["favoriteList"]
 
 # Descarga cada uno de los videos con yt-dlp
-for element in video_elements:
-    # Obtén la URL del video
-    video_url = element.get_attribute("href")
+for video in videos:
+    os.system(f"yt-dlp -o videos/{video['title']} {video['video']['downloadAddr']}")
+
+# Si hay más páginas de videos, obtén los siguientes
+while response.json()["hasMore"]:
+    # Obtén el ID del último video de la lista
+    max_cursor = response.json()["maxCursor"]
     
-    # Descarga el video con yt-dlp
-    os.system(f"yt-dlp -o videos/{video.title} {video_url}")
+    # Realiza la solicitud a la API para obtener la siguiente página de videos
+    params["max_cursor"] = max_cursor
+    response = requests.get(url, headers=headers, params=params)
+    
+    # Obtén los datos de los videos de la respuesta
+    videos = response.json()["favoriteList"]
 
-# Cierra el navegador
-driver.quit()
-
-# Finaliza la sesión de TikTok
-tiktok.logout()
+    # Descarga cada uno de los videos con yt-dlp
+    for video in videos:
+        os.system(f"yt-dlp -o videos/{video['title']} {video['video']['downloadAddr']}")
