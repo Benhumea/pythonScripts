4469bad9d46d76187e59556bf9353b767666c950
Jose Benhumea <joseramosb@gmail.com>
2023-01-06 23:52:44 -0600

    Version 0.0.2 fallida es la que utiliza yt-dlp, solo hay que cambiar la syntaxys de yt-dlp

--- ttDownloadLV/ttdlv.py
+++ ttDownloadLV/ttdlv.py
@@ -1,56 +1,42 @@
-import requests
-import os
+# Importa la librería de manejo de errores de HTTP
+import requests.exceptions
 
-# Obtén tu "client key" y "client secret" de la consola de desarrolladores de TikTok
-CLIENT_KEY = "aw6pq0ii3ved3nox"
-CLIENT_SECRET = "59b0d9b483dc41ac515377e3a5bc62f8"
+# Intenta descargar los videos
+try:
+    # Obtén tu "client key" y "client secret" de la consola de desarrolladores de TikTok
+    CLIENT_KEY = "aw6pq0ii3ved3nox"
+    CLIENT_SECRET = "59b0d9b483dc41ac515377e3a5bc62f8"
 
-# Solicita un "access token" a la API de TikTok
-response = requests.post("https://api.tiktok.com/oauth/token", {
-    "client_key": CLIENT_KEY,
-    "client_secret": CLIENT_SECRET,
-    "grant_type": "client_credentials",
-    "scope": "favorite"
-})
+    # Solicita un "access token" a la API de TikTok
+    response = requests.post("https://api.tiktok.com/oauth/token", {
+        "client_key": CLIENT_KEY,
+        "client_secret": CLIENT_SECRET,
+        "grant_type": "client_credentials",
+        "scope": "favorite"
+    })
 
-# Obtén el "access token" de la respuesta
-access_token = response.json()["access_token"]
+    # Obtén el "access token" de la respuesta
+    access_token = response.json()["access_token"]
 
-# Crea una carpeta para guardar los videos descargados
-if not os.path.exists("videos"):
-    os.makedirs("videos")
+    # Crea una carpeta para guardar los videos descargados
+    if not os.path.exists("videos"):
+        os.makedirs("videos")
 
-# Obtén la lista de videos que le has dado "me gusta" en tu cuenta de TikTok
-url = "https://api.tiktok.com/favorite/list"
-headers = {
-    "Authorization": f"Bearer {access_token}"
-}
-params = {
-    "count": "100"  # Solicita 100 videos por página
-}
+    # Obtén la lista de videos que le has dado "me gusta" en tu cuenta de TikTok
+    url = "https://api.tiktok.com/favorite/list"
+    headers = {
+        "Authorization": f"Bearer {access_token}"
+    }
+    params = {
+        "count": "100"  # Solicita 100 videos por página
+    }
 
-# Realiza la solicitud a la API
-response = requests.get(url, headers=headers, params=params)
-
-# Obtén los datos de los videos de la respuesta
-videos = response.json()["favoriteList"]
-
-# Descarga cada uno de los videos con yt-dlp
-for video in videos:
-    os.system(f"yt-dlp -o videos/{video['title']} {video['video']['downloadAddr']}")
-
-# Si hay más páginas de videos, obtén los siguientes
-while response.json()["hasMore"]:
-    # Obtén el ID del último video de la lista
-    max_cursor = response.json()["maxCursor"]
-    
-    # Realiza la solicitud a la API para obtener la siguiente página de videos
-    params["max_cursor"] = max_cursor
+    # Realiza la solicitud a la API
     response = requests.get(url, headers=headers, params=params)
-    
+
     # Obtén los datos de los videos de la respuesta
     videos = response.json()["favoriteList"]
 
     # Descarga cada uno de los videos con yt-dlp
     for video in videos:
-        os.system(f"yt-dlp -o videos/{video['title']} {video['video']['downloadAddr']}")
+        os.system(f"yt-dlp -o videos/{video['title']
